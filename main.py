1bec34cd1dd911b3aa2fe585276c4993db1914df
Daniel Zhao <zhaodaniel2005@gmail.com>
2024-01-11 01:32:23 -0500

    fixed, now gestures is complete

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import cv2
 import tensorflow as tf
 import mediapipe as mp
+from mediapipe.tasks.python import vision
 import numpy as np 
 import time
 
 import Exercises
 from GameStatesEnum import GameStates
 
-# mp models setup
+# mediapipe setup
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 mp_pose = mp.solutions.pose
 
 pose = mp_pose.Pose(min_detection_confidence=0.60, min_tracking_confidence=0.80)
 hands = mp_hands.Hands(min_detection_confidence=0.50, min_tracking_confidence=0.70)
+
+# mediapipe model setup
+gesture_path = "models/gesture_recognizer.task"
+
+gesture_option = mp.tasks.BaseOptions(model_asset_path=gesture_path)
+
+gesture_options = mp.tasks.vision.GestureRecognizerOptions(
+    base_options=gesture_option)
+recognizer = mp.tasks.vision.GestureRecognizer.create_from_options(gesture_options)
 
 game_state = GameStates.MAIN_MENU
 READY_TIME = 3
@@ -22,9 +32,11 @@ ready_cnt = 0
 # setup cmaera
 cam = cv2.VideoCapture(0)
 start = time.time()
+elapsed_time = 0
 while cam.isOpened():
     ret, frame = cam.read()
     delta_time = time.time() - start
+    elapsed_time += delta_time
     start = time.time()
 
     # get keyboard input
@@ -38,18 +50,23 @@ while cam.isOpened():
 
     if game_state == GameStates.MAIN_MENU:
         # proccess image through hand detection
-        # crop photo in half horizontally
+        # blackout half of the image, this way the dectection positions are accurate
         # if both hands are giving thumbs up, then move to next screen.
 
-        image_left = cv2.cvtColor(image[ :, :image.shape[1]//2], cv2.COLOR_BGR2RGB)
+        image_left = np.copy(image)
+        image_left = cv2.cvtColor(image_left, cv2.COLOR_BGR2RGB)
+        image_left[ :, image.shape[1]//2:, ] = [0, 0, 0]
         image_left.flags.writeable = False
 
-        image_right = cv2.cvtColor(image[ :, image.shape[1]//2:], cv2.COLOR_BGR2RGB)
+        image_right = np.copy(image)
+        image_right = cv2.cvtColor(image_right, cv2.COLOR_BGR2RGB)
+        image_right[ :, :image.shape[1]//2, ] = [0, 0, 0]
         image_right.flags.writeable = False
         
         hands_results_left = hands.process(image_left)
-        hands_results_right = hands.process(np.ascontiguousarray(image_right))
+        hands_results_right = hands.process(image_right)
 
+        image.flags.writeable = True
         if hands_results_left.multi_hand_landmarks:
             landmarks = []
             for handslms in hands_results_left.multi_hand_landmarks:
@@ -61,15 +78,28 @@ while cam.isOpened():
                     landmarks.append([lmx, lmy])
 
                 # Drawing landmarks on frames
-                #mp_drawing.draw_landmarks(image, handslms, mp_hands.HAND_CONNECTIONS)
-                    
-        image.flags.writeable = True
-        if hands_results_left.multi_hand_landmarks:
-            for num, hand in enumerate(hands_results_left.multi_hand_landmarks):
-                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
-                                        mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
-                                        mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2),
-                                        )
+                mp_drawing.draw_landmarks(image, handslms, mp_hands.HAND_CONNECTIONS)
+
+        if hands_results_right.multi_hand_landmarks:
+            landmarks = []
+            for handslms in hands_results_right.multi_hand_landmarks:
+                for lm in handslms.landmark:
+                    # print(id, lm)
+                    lmx = int(lm.x * image.shape[1])
+                    lmy = int(lm.y * image.shape[1])
+
+                    landmarks.append([lmx, lmy])
+
+                # Drawing landmarks on frames
+                mp_drawing.draw_landmarks(image, handslms, mp_hands.HAND_CONNECTIONS)
+
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+        gesture_recognition_result = recognizer.recognize(mp_image)
+        
+        try:
+            print(gesture_recognition_result.gestures[0][0])
+        except:
+            pass
 
     elif game_state == GameStates.PLAYING:
         pass
